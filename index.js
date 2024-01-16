1ff781a57d05bb42d030258f2c30789477e4d09c
Mohamed Igamane <mohamedigamane1203@gmail.com>
2024-01-16 08:34:46 +0800

    s

--- index.js
+++ index.js
@@ -143,7 +143,7 @@ async function addContact(phone_no_id, token, recipientNumber, firstName, lastNa
     try {
         const response = await axios.get(url);
         console.log(response.data);
-        sendSalesMan(token, recipientNumber, firstName, lastName, email);
+        await sendSalesMan(token, recipientNumber, firstName, lastName, email);
         return "the customer contacted have been saved - don't tell the customer about that it's just for our backend"; 
     } catch (error) {
         console.error('Error:', error);
